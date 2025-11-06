02634c1a00bad966667aa89041de0f4e99406069
Sarah Karoline <sarah_ferreira07@hotmail.com>
2025-11-06 16:36:42 -0300

    Projeto Calculadora producao

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double a =5;
+        double b =6;
+
+        double media = a+b/2;
+        System.out.println("Essa é sua media:" + media);
+        System.out.println("Helloword");
+
+    }
+}
